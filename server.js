cf9e0c738e272ec34dadfa9c44770f3df28f5d0d
Aniket <surveaniket461@gmail.com>
2019-05-01 21:19:40 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const path = require('path');
-const express = require('express');
+const path = require('path');//module to join path to the public folder
+const express = require('express'); // express module which will help develop server easily
 
 
 
-const publicPath = path.join(__dirname, '/public');
-var app = express();
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '/public'); // joined the path to public folder
+var app = express(); // server created
+const port = process.env.PORT || 3000; // where we want to access our server
 
 app.use(express.static(publicPath));
 
 
+
+// telling server to listen on above given port
 app.listen(port, function(){
 
 	console.log("Server is up on port: " +port);
